35cdaed602d60b04857a9ee583e40fd58c80acc8
SahidChairulf <sahidchairulf@gmail.com>
2023-08-23 08:54:15 +0700

    GreenGrosir

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <NavbarCompponent></NavbarCompponent>
+  <RouterView></RouterView>
+</template>
+
+  <script setup>
+import { RouterView } from 'vue-router';
+import NavbarCompponent from './components/NavbarCompponent.vue'
+
+  </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <HeaderCompponent></HeaderCompponent>
+</template>
+<script setup>
+  import HeaderCompponent from '../components/HeaderCompponent.vue';
+</script>
